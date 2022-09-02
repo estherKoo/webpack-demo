55ebb093b4be3206c9714c6a45d2c8f5f2ca43af
zoni <465181200@qq.com>
2022-09-02 15:16:56 +0800

    feat: 加载 images 图像

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import _ from 'lodash';
 import './style.css'
+import Kitty from './kitty.jpeg'
 
 function component() {
   const element = document.createElement('div');
   // lodash 在当前 script 中使用 import 引入
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   element.classList.add('hello');
+
+  // 将图像添加到我们已经存在的 div 中。
+  const myImg = new Image();
+  myImg.src = Kitty;
+
+  element.appendChild(myImg);
+
   return element;
 }
     
